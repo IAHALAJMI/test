7de2794164292a8442064469d2b130043cb05b5d
IAHALAJMI <iahalajmi@uclan.ac.uk>
2023-01-26 14:15:47 +0000

    version1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "platform/mbed_thread.h"
+
+
+// Blinking rate in milliseconds
+#define timeled                                                    500
+
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        thread_sleep_for(timeled);
+    }
+}
